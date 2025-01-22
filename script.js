b62219d0e784363e415d1da61fa582d8c0c4afd4
kingmakerallof <kingmakerallof@gmail.com>
2025-01-22 21:33:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const navtab = document.getElementById('nav-tab');
+
+navtab.addEventListener('click', tabclick);
+
+function tabclick(event) {
+    //Get all The active tab
+    const activeTabs = document.querySelectorAll('.active');
+
+    activeTabs.forEach((tab) => {
+        tab.classList.remove('active');
+    });
+
+    const pages = document.querySelectorAll('.page');
+    pages.forEach((page) => {
+        page.classList.remove('page-active');
+    });
+
+    // event.target.parentElement.classList.add('active');
+    event.target.parentElement.className += 'active';
+
+    let id = event.target.href.split("#")[1];
+    const page = document.getElementById(id);
+    page.classList.add('page-active');
+}
